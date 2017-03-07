a826000b7a90fe862c2a66e6770775cf390fb2a3
Olha <yershova.olga@gmail.com>
2017-03-07 15:45:47 +0200

    simple js

---
+++ s.js
@@ -0,0 +1,12 @@
+click_car.onclick = function() { 
+var dd = document.getElementById('top_block');
+dd.style.transition = 'background 1s ease';
+dd.style.background = 'url(img/ddd.jpg) no-repeat';
+};
+
+click_an_car.onclick = function() { 
+var dd = document.getElementById('top_block');
+dd.style.transition = 'background 1s ease';
+dd.style.background = 'url(img/bbb.jpg) no-repeat';
+
+};
